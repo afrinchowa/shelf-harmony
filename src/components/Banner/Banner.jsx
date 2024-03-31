84cb0aaf1771c9eced30a3951cf7570dc7ed0b02
Chowa <arfinchowa524@gmail.com>
2024-03-31 23:59:55 +0600

    button updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,19 @@
-
+import { Link } from "react-router-dom";
 
 const Banner = () => {
   return (
+    // <div className="w-[78rem] h-[34rem] bg-[#1313130D] mx-auto rounded-xl" >
+    //     <div>
+    //         <div>
+    //             <h2 className="text-4xl">Books to freshen up your bookshelf</h2>
+    //            <Link to='/listed'> <button className="bg-[#23BE0A] text-white w-[11rem] h-[4rem] font-bold work-sans rounded-xl mt-4">View The List</button></Link>
+    //         </div>
+    //         <div>
+    //             <img src="" alt="" />
+    //         </div>
+    //     </div>
+
+    // </div>
     <div
       className="hero min-h-screen"
       style={{
@@ -18,7 +30,12 @@ const Banner = () => {
             excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
             a id nisi.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <Link to="/listed">
+            {" "}
+            <button className="bg-[#23BE0A] text-white w-[11rem] h-[4rem] font-bold work-sans rounded-xl mt-4">
+              View The List
+            </button>
+          </Link>
         </div>
       </div>
     </div>
