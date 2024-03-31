24087bab1265c5502f1ad5c54e7d20b29cda080d
Chowa <arfinchowa524@gmail.com>
2024-03-31 23:57:32 +0600

    banner picture added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,21 +1,28 @@
-import { Link } from "react-router-dom";
 
 
 const Banner = () => {
-    return (
-        <div className="w-[78rem] h-[34rem] bg-[#1313130D] mx-auto rounded-xl" >
-            <div>
-                <div>
-                    <h2 className="text-4xl">Books to freshen up your bookshelf</h2>
-                   <Link to='/listed'> <button className="bg-[#23BE0A] text-white w-[11rem] h-[4rem] font-bold work-sans rounded-xl mt-4">View The List</button></Link>
-                </div>
-                <div>
-                    <img src="" alt="" />
-                </div>
-            </div>
-            
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://i.ibb.co/JCgj31Z/kelly-sikkema-AOGo3-DHi3s-I-unsplash.jpg)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
