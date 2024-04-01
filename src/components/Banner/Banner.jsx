5e1e2f3c5bf7d0b7c3d5c14c58b1ac59a579c3ef
Chowa <arfinchowa524@gmail.com>
2024-04-01 23:53:28 +0600

    banner updated

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -24,11 +24,9 @@ const Banner = () => {
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold">Explore Your Inner World Through Books</h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+          Welcome to SelfHarmony, where the journey of self-discovery begins with the turn of a page. Immerse yourself in a sanctuary of words, where every book is a gateway to understanding, growth, and harmony. 
           </p>
           <Link to="/listed">
             {" "}
